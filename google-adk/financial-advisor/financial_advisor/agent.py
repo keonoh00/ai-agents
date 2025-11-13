57ff235ce024c600ec8dd224266db85e51ab6b66
keonoh00 <keonoh00@gmail.com>
2025-11-13 11:24:16 +0900

    feat: integrate OllamaLLM into financial advisor agents and update dependencies

--- google-adk/financial-advisor/financial_advisor/agent.py
+++ google-adk/financial-advisor/financial_advisor/agent.py
@@ -3,17 +3,17 @@ import os
 import dotenv
 from google.adk.agents import Agent
 from google.adk.tools.agent_tool import AgentTool
+from ollama import OllamaLLM
 
 from .data_analyst import data_analyst
 from .financial_analyst import financial_analyst
 from .news_analyst import news_analyst
-from .ollama import OllamaLLM
 from .prompt import PROMPT
 
 dotenv.load_dotenv(dotenv_path="../../../.env")
 
 
-llm = OllamaLLM(model="ollama/gpt-oss:latest")
+llm = OllamaLLM(model="ollama/gpt-oss:latest").googleAdk()
 
 
 def save_advice_report():
@@ -23,7 +23,7 @@ def save_advice_report():
 agent = Agent(
     name="FinancialAdvisor",
     instruction=PROMPT,
-    model=llm.googleAdk(),
+    model=llm,
     tools=[
         AgentTool(agent=financial_analyst),
         AgentTool(agent=news_analyst),
