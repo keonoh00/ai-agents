3786f82ce1a288e63d171d33acda86fb21a523f8
keonoh00 <keonoh00@gmail.com>
2025-11-17 10:13:10 +0900

    feat: callback example

--- google-adk/youtube-shorts-maker/youtube_shorts_maker/agent.py
+++ google-adk/youtube-shorts-maker/youtube_shorts_maker/agent.py
@@ -1,5 +1,9 @@
 from google.adk.agents import Agent
+from google.adk.agents.callback_context import CallbackContext
+from google.adk.models.llm_request import LlmRequest
+from google.adk.models.llm_response import LlmResponse
 from google.adk.tools.agent_tool import AgentTool
+from google.genai import types
 from ollama_wrapper import OllamaLLM
 
 from .prompt import SHORTS_PRODUCER_DESCRIPTION, SHORTS_PRODUCER_PROMPT
@@ -7,7 +11,29 @@ from .sub_agents.asset_generator.agent import asset_generator
 from .sub_agents.content_planner.agent import content_planner
 from .sub_agents.video_assembler.agent import video_assembler
 
+
+def before_model_callback(
+    callback_context: CallbackContext,
+    llm_request: LlmRequest,
+):
+
+    history = llm_request.contents
+    last_message = history[-1]
+    if last_message.role == "user" and last_message.parts:
+        text = last_message.parts[0].text
+        if text and "hummus" in text:
+            return LlmResponse(
+                content=types.Content(
+                    parts=[types.Part(text="Sorry, I cannot help you with that...")],
+                    role="model",
+                )
+            )
+
+    return None
+
+
 llm = OllamaLLM(model="ollama/gpt-oss:latest").googleAdk()
+
 shorts_producer = Agent(
     name="ShortsProducerAgent",
     description=SHORTS_PRODUCER_DESCRIPTION,
@@ -18,6 +44,7 @@ shorts_producer = Agent(
         AgentTool(agent=asset_generator),
         AgentTool(agent=video_assembler),
     ],
+    before_model_callback=before_model_callback,
 )
 
 root_agent = shorts_producer
